81b3cbb7250959c7e6ab66507f634fb1e8448d78
Vahid Rafiei <vahid.rafiei@gmail.com>
2013-06-19 18:39:29 +0200

    changing the date of license

--- analyser.py
+++ analyser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 ''' 
-Copyright (C) 2012  Vahid Rafiei (@vahid_r)
+Copyright (C) 2013  Vahid Rafiei (@vahid_r)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
